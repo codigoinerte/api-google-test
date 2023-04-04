c26cf1cdc802d609dec1bef86d8b2bc151172048
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-04-04 12:00:25 -0500

    cambio de headers

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
 header('X-Fowarded-With: XmlHttpRequest');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
